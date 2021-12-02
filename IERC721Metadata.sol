117e39ae649b1018a85d075564831ecdba72007d
vatsal2312 <83644350+vatsal2312@users.noreply.github.com>
2021-12-02 20:03:11 +0530

    Add files via upload

--- IERC721Metadata.sol
+++ IERC721Metadata.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.4.0 (token/ERC721/extensions/IERC721Metadata.sol)
 
 pragma solidity ^0.8.0;
 
-import "./IERC721.sol";
+import "../IERC721.sol";
 
 /**
  * @title ERC-721 Non-Fungible Token Standard, optional metadata extension
